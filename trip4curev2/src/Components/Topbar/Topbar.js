d5f4a5f8034d63b8b88669bde99f09f4b5cef1f2
Rakshitmv <rakshit8976646711@gmail.com>
2023-06-22 11:30:15 +0530

    22-06-23 updated 11:30 AM

--- trip4curev2/src/Components/Topbar/Topbar.js
+++ trip4curev2/src/Components/Topbar/Topbar.js
@@ -51,50 +51,53 @@ function Topbar() {
     }
    
     return (
-      <section>
-        <Container>
-            <Row className="align-items-center">
-                <Col md={8}>
-                    <div className="d-flex align-items-center">
-                        <div className="news-title me-3"><img src={`${process.env.PUBLIC_URL}/images/ic-announcement.png`} style={{height:20}}  alt="" /><span className="ps-2 d-md-inline-block d-none">Announcement</span></div>
-                        <NewsTicker
-                        rowHeight = {10}
-                        maxRows = {2}
-                        speed = {600}
-                        direction = {Directions.UP}
-                        duration = {4000}
-                        autoStart = {true}
-                        pauseOnHover = {true}
-                        id = "nt-title"
-                        className = "myClassName1 myClassName2">
-                        <div>Using a medical tourism facilitator.</div>
-                        <div>Curabitur porttitor ante eget hendrerit adipiscing.</div>
-                        <div>Praesent ornare nisl lorem, ut condimentum lectus gravida ut.</div>
-                        <div>Nunc ultrices tortor eu massa placerat posuere.</div>
-                        </NewsTicker>
-                    </div>
-                </Col>
-                <Col md={4}>
-                    <div className="navbar-other d-flex justify-content-md-end justify-content-center">
-                    <Nav className="nav social social-white">
-                        <Link to={''}><i className="fa fa-twitter"></i></Link>
-                        <Link to={''}><i className="fa fa-facebook"></i></Link>
-                        <Link to={''}><i className="fa fa-linkedin"></i></Link>
-                        <Link to={''}><i className="fa fa-instagram"></i></Link>
-                    </Nav>
-                    <NavDropdown title="ENGLISH" className="navbar-nav flex-row align-items-center  langauge" id="collasible-nav-dropdown">
-                        <NavDropdown.Item onClick={handleLngChange} lang="en">ENGLISH</NavDropdown.Item>
-                        <NavDropdown.Item onClick={handleLngChange} lang="es">SPANISH</NavDropdown.Item>
-                        <NavDropdown.Item onClick={handleLngChange} lang="ar">ARABIC</NavDropdown.Item>
-    </NavDropdown>
-    <div id="google_translate_element"></div>
-                    
-                
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-      </section>
+        <>
+            <section>
+                <Container>
+                    <Row className="align-items-center">
+                        <Col md={8}>
+                            <div className="d-flex align-items-center">
+                                <div className="news-title me-3"><img src={`${process.env.PUBLIC_URL}/images/ic-announcement.png`} style={{height:20}}  alt="" /><span className="ps-2 d-md-inline-block d-none">Announcement</span></div>
+                                <NewsTicker
+                                rowHeight = {10}
+                                maxRows = {2}
+                                speed = {600}
+                                direction = {Directions.UP}
+                                duration = {4000}
+                                autoStart = {true}
+                                pauseOnHover = {true}
+                                id = "nt-title"
+                                className = "myClassName1 myClassName2">
+                                <div>Using a medical tourism facilitator.</div>
+                                <div>Curabitur porttitor ante eget hendrerit adipiscing.</div>
+                                <div>Praesent ornare nisl lorem, ut condimentum lectus gravida ut.</div>
+                                <div>Nunc ultrices tortor eu massa placerat posuere.</div>
+                                </NewsTicker>
+                            </div>
+                        </Col>
+                        <Col md={4}>
+                            <div className="navbar-other d-flex justify-content-md-end justify-content-center">
+                                <Nav className="nav social social-white">
+                                    <Link to={''}><i className="fa fa-twitter"></i></Link>
+                                    <Link to={''}><i className="fa fa-facebook"></i></Link>
+                                    <Link to={''}><i className="fa fa-linkedin"></i></Link>
+                                    <Link to={''}><i className="fa fa-instagram"></i></Link>
+                                </Nav>
+                                <NavDropdown title="ENGLISH" className="navbar-nav  align-items-center  langauge" id="collasible-nav-dropdown">
+                                    <NavDropdown.Item onClick={handleLngChange} lang="en">ENGLISH</NavDropdown.Item>
+                                    <NavDropdown.Item onClick={handleLngChange} lang="es">SPANISH</NavDropdown.Item>
+                                    <NavDropdown.Item onClick={handleLngChange} lang="ar">ARABIC</NavDropdown.Item>
+                                </NavDropdown>
+                                <div id="google_translate_element"></div>
+                            
+                        
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </>
+     
     )
   }
   
